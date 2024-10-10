e8bc419937671e6a2211b3ad8429c0199f89e0fe
pereira3r <anthony_rodriguespereira@outlook.com>
2024-10-10 14:16:01 -0400

    Start activity of design patterns

---
+++ desafio-dio-design-patterns/src/main/java/com/example/desafio_dio_design_patterns/DesafioDioDesignPatternsApplication.java
@@ -0,0 +1,13 @@
+package com.example.desafio_dio_design_patterns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioDioDesignPatternsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioDioDesignPatternsApplication.class, args);
+	}
+
+}
---
+++ desafio-dio-design-patterns/src/test/java/com/example/desafio_dio_design_patterns/DesafioDioDesignPatternsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.desafio_dio_design_patterns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioDioDesignPatternsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
