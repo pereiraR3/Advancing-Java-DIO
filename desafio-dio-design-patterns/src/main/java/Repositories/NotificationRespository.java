09fb0babe98d3af20f324ae81e73c2eb79ccda5a
Anthony | Developer Back-End <159745739+pereiraR3@users.noreply.github.com>
2024-10-16 23:18:57 -0400

    Update NotificationRespository.java

--- desafio-dio-design-patterns/src/main/java/Repositories/NotificationRespository.java
+++ desafio-dio-design-patterns/src/main/java/Repositories/NotificationRespository.java
@@ -3,6 +3,7 @@ package Repositories;
 import Models.Notification.NotificationModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+@Builder
 public interface NotificationRespository extends JpaRepository<NotificationModel, Long> {
 
 }
